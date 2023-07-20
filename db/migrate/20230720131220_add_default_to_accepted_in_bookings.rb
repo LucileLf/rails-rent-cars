d8a5482c7698abe965a40704673276e308661f45
Lucile Lefebvre <lucile.c.lefebvre@gmail.com>
2023-07-20 15:14:56 +0200

    models and validations

---
+++ db/migrate/20230720131220_add_default_to_accepted_in_bookings.rb
@@ -0,0 +1,5 @@
+class AddDefaultToAcceptedInBookings < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default :bookings, :accepted, false
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'cleaning database'
+Booking.destroy_all
+Car.destroy_all
+User.destroy_all
+puts 'database cleaned'
+
+puts 'creating users'
+toto = User.create!(
+  email: 'toto@gmail.com',
+  password: '123456')
+moi = User.create!(
+  email: 'lucile@gmail.com',
+  password: '123456')
+puts 'users created'
+
+puts 'creating cars'
+5.times do
+  Car.create!(
+    brand: Faker::Vehicle.make,
+    model: Faker::Vehicle.model,
+    year_of_production: rand(2004..2022),
+    address: Faker::Address.full_address,
+    price_per_day: rand(60..120),
+    user: toto
+  )
+end
+puts 'cars created'
+
+puts 'creating bookings'
+Booking.create!(
+  car: Car.last,
+  user: moi,
+  starts_at: Date.today,
+  ends_at: Date.today + 3
+)
+puts 'bookings created'
